c8b94f2329571e99412b95505d1424c49c3c9cf7
zienab20190463 <74000589+zienab20190463@users.noreply.github.com>
2022-08-15 17:51:09 +0200

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,158 @@
+
+//number 1
+/*let num=Number(prompt("enter number"))
+if(num%2==0){
+    console.log("even");
+}
+else{
+
+
+console.log("odd");}*/
+
+//number 2
+/*function FizzBuzz(){
+let num=Number(prompt("enter number"))
+    if (num % 15 == 0)
+        console.log("FizzBuzz");
+    else if (num % 3 == 0)
+        console.log("Fizz");
+    else if (num  % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(num);
+}
+FizzBuzz()*/
+
+//number 3
+/*let s=prompt("enter string");
+for(var i=s.length;i>0;i--){
+    console.log(s.charAt(i))
+} */
+
+//number 4
+/*let r=Number(prompt("enter radius"))
+const pi=3.14;
+let area=pi*(Math.pow(r,2));
+let circ=2*pi*r;
+console.log("area of circle = "+area);
+console.log(`circumference of circle is ${circ}`);*/
+
+//number 5
+
+/*let x=Number(prompt("enter first num"));
+let y=Number(prompt("enter second num"));
+if(x==50||y==50){
+    console.log("true");
+}
+else if(x+y==50){
+    console.log("true");
+}*/
+
+//number 6
+/*let x=Number(prompt("enter first num"));
+let y=Number(prompt("enter second num"));
+if(x<0){
+    console.log("1st num  is negative");
+}else{
+    console.log("1st num  is positive");
+}
+if(y<0){
+    console.log("2st num  is negative");
+}else{
+    console.log("2st num  is positive");
+}*/
+
+//number 7
+/*let x=parseInt(prompt("enter num"));
+if(x%8==0){
+    console.log(" num  is mult of 8");
+}
+else if(x%5==0){
+    console.log(" num  is mult of 5");
+}
+else{
+    console.log(" num  is  not mult of 5 nor mult of 8");
+}*/
+
+//number 8
+/*let x=Number(prompt("enter first num"));
+let y=Number(prompt("enter second num"));
+let z=Number(prompt("enter third num"));
+
+if(x>y&&x>z){
+    console.log(" largest is 1st "+x);
+}
+else if(y>x&&y>z){
+    console.log(" largest is 2nd  "+y);
+}
+else if(z>x&&z>y){
+    console.log(" largest is 3rd "+z);
+}*/
+
+//number 9
+/*let sum=0;
+for(var i=1;i<=10;i++){
+    sum+=i;
+}
+console.log(`sum= ${sum}`);*/
+
+//number 10
+
+/*for(var i=1;i<=5;i++){
+    for(var j=1;j<=i;j++){
+        console.log("*");
+
+    }
+    console.log("\n");
+
+}*/
+
+//number 11
+
+/*let x=Number(prompt("enter number"));
+if(x<0){
+    console.log("negative")
+}
+else{
+    console.log("positive")
+}*/
+
+//number 12
+/*var arr=[];
+for(var i=1;i<=10;i++){
+arr.splice(i,0,i)}
+console.log(arr);*/
+//number 13
+
+/*let x=Number(prompt("enter first num"));
+let y=Number(prompt("enter second num"));
+console.log(`sum= ${x+y}`);*/
+
+//number 14
+
+/*let x=Number(prompt("enter num"));
+let fact=1;
+for(var i=1;i<=x;i++){
+fact*=i;
+}
+console.log(fact);*/
+
+//number 15
+/*let x=Number(prompt("enter first num"));
+let y=Number(prompt("enter second num"));
+let op=(prompt("enter operation "));
+switch(op){
+case '+': console.log(x+y);
+    break;
+case '-': console.log(x-y);
+    break;
+case '*': console.log(x*y);
+    break;
+    
+case '/': console.log(x/y);
+    break;
+ default: console.log("invalid operation")   
+    
+}*/
+
+
